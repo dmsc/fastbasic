ee29f170df8d44e1e138dd15e28040db649790aa
Daniel Serpell <daniel.serpell@gmail.com>
2025-01-12 22:58:22 -0300

    Small improvements to the make-release script.
    - Create the output directory if not exists,
    - Use the sample template for both manuals,
    - Don't compile for OSX or WIN32 if the compilers are missing.
    
    This fixes #103.

--- make-release.sh
+++ make-release.sh
@@ -21,8 +21,13 @@ out="$rdir/fastbasic-$ver"
 LTO_FLAGS="-flto -flto-partition=none"
 CXX_FLAGS="-DNDEBUG -Os -Wall"
 LIN64_FLAGS="$CXX_FLAGS $LTO_FLAGS -static-libstdc++ -Wl,--gc-sections"
+WIN_CROSS=i686-w64-mingw32-
 WIN_FLAGS="$CXX_FLAGS -static -Wl,--gc-sections"
+OSX64_CROSS=x86_64-apple-darwin20.2-
 OSX64_FLAGS="$CXX_FLAGS -flto -static-libstdc++"
+
+# Make output folder if not exists
+mkdir -p "$rdir"
 
 echo "----------- Compiling release $ver -------------"
 echo ""
@@ -36,21 +41,40 @@ compile_lin64() {
 }
 
 compile_win32() {
+    if ! which "${WIN_CROSS}gcc" > /dev/null ; then
+        echo "-----------------------------------------------------"
+        echo "--"
+        echo "-- ERROR: missing windows compiler ${WIN_CROSS}gcc"
+        echo "--"
+        echo "-----------------------------------------------------"
+        return
+    fi
+
     # Compile with mingw-w64 cross compiler to 32bit:
-    make $ncpu CROSS=i686-w64-mingw32- TARGET_OPTFLAGS="$WIN_FLAGS" build/fastbasic.zip
+    make $ncpu CROSS="$WIN_CROSS" TARGET_OPTFLAGS="$WIN_FLAGS" build/fastbasic.zip
     mv build/fastbasic.zip "${out}-win32.zip"
-    make CROSS=i686-w64-mingw32- distclean
+    make CROSS="$WIN_CROSS" distclean
 }
 
 compile_osx() {
     OPATH="$PATH"
     PATH="$PATH:/opt/osx/bin"
+
+    if ! which "${OSX64_CROSS}cc" > /dev/null ; then
+        echo "-----------------------------------------------------"
+        echo "--"
+        echo "-- ERROR: missing OSX compiler ${OSX64_CROSS}cc"
+        echo "--"
+        echo "-----------------------------------------------------"
+        return
+    fi
     # Compile FAT binary for OSX.
     # Note that this is simpler with CLANG, but it produces a binary slower and twice the size!
     #  First compile to 64bit:
-    make $ncpu CROSS=x86_64-apple-darwin20.2- CC=cc CXX=c++ TARGET_OPTFLAGS="$OSX64_FLAGS" build/fastbasic.zip
+    make $ncpu CROSS="$OSX64_CROSS" CC=cc CXX=c++ TARGET_OPTFLAGS="$OSX64_FLAGS" \
+         build/fastbasic.zip
     mv build/fastbasic.zip "${out}-macosx.zip"
-    make CROSS=x86_64-apple-darwin20.2- CC=cc CXX=c++ distclean
+    make CROSS="$OSX64_CROSS" CC=cc CXX=c++ distclean
     PATH="$OPATH"
 }
 
@@ -68,5 +92,5 @@ pandoc build/compiler/MANUAL.md -o "${out}-manual.pdf" \
 
 make build/compiler/USAGE.md
 pandoc build/compiler/USAGE.md -o "${out}-cross-compiler.pdf" \
-       --from markdown-raw_tex --template ~/eisvogel.tex --listings
+       --from markdown-raw_tex --template template.tex --listings
 
